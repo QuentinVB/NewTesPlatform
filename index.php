15a55f1a4d8cada81ecacf8aabc567696d657ff2
QVanbutsele <vanbutsele@intechinfo.fr>
2016-04-12 16:43:12 +0200

    update main

--- index.php
+++ index.php
@@ -7,16 +7,19 @@
 </head>
 <body>
 	<div id="header">
+	<h1>Titre de la page</h1>
 		<?php 
 			include("header.php");
 		?>
 	</div>
 	<div id="body">
+	<p>Corps de la page, avec les trucs a mettre</p>
 		<?php 
 			include("body.php");
 		?>
 	</div>
 	<div id="body">
+	<p>Le pied, tout en bas o_o</p>
 		<?php 
 			include("footer.php");
 		?>
